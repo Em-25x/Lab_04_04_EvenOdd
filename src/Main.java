59e642a25591cd060c1560f2a7a2c44e579a5e1c
Em-25x <emmathehunter2007@gmail.com>
2025-09-15 23:39:03 -0400

    All done!

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //A program that takes an integer (java int) and uses the modulo function to indicate if the number is odd or even.
+        // Since we can't do input in java yet, just hard code the value for the input as a variable instantiation.
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int favoriteNumber = 3;
+        int moddedNumber = 5;
+
+        moddedNumber = favoriteNumber % 2;
+
+        System.out.println("If your mod is 1, then your number was odd. If your mod is 0, then your number was even. Your mod: "
+        + moddedNumber);
     }
 }
